ff459e5b0dba50f0146e2c811b342161a9b6ca21
Michal Banik <banik.michal.sk@gmail.com>
2015-10-26 00:24:52 +0100

    remove debug outputs when testing

--- src/sk/banik/fri/dataStructures/tests/TestRBTree.java
+++ src/sk/banik/fri/dataStructures/tests/TestRBTree.java
@@ -91,11 +91,6 @@ public class TestRBTree {
 		HashMap<Integer, String> backupStructure = new HashMap<Integer, String>();
 		int maxTestedCount = 100;
 		
-		// DEBIG
-//		LinkedList<Integer> inputs = new LinkedList<Integer>();
-//		inputs.addAll(Arrays.asList(0,1285,4622,1812,1557,7966,1826,6947,5156,6180,6438,9510,5927,42,2348,5423,5938,1591,9015,1849,7231,4416,6464,3392,4673,68,5956,9552,1617,2388,9049,1113,5978,5979,9308,5982,2146,6754,3430,3688,4972,621,8046,6003,3702,7031,6268,4989,2688,6784,641,9346,5252,2437,2949,2951,5768,3982,4750,3983,3984,8595,3989,3228,413,5022,8862,2975,7071,3244,8620,8111,5554,5042,703,3264,2756,4037,9413,2501,4807,4296,2249,1482,7627,8909,3533,3535,3792,988,6623,8672,4321,6371,230,9193,5866,8436,4598,9466));
-		//
-		
 		do {
 			// insert pair <key, value>
 			Double dKey = (Math.random() * maxTestedCount * maxTestedCount);
@@ -103,75 +98,31 @@ public class TestRBTree {
 				dKey = (Math.random() * maxTestedCount * maxTestedCount);
 			}
 			
-			// DEBIG
-//			dKey = new Double(inputs.remove(0));
-			//
-			
 			String val = "" + dKey.intValue();
 			rbTree.insert(dKey.intValue(), val);
 			backupStructure.put(dKey.intValue(), val);
 		} while (backupStructure.size() < maxTestedCount);
 		
-		// DEBIG
-//		rbTree.printBetter();
-//		List<Integer> list = new LinkedList<Integer>();
-//		list.addAll(backupStructure.keySet());
-////		printList(list);
-//		int lastKey = -1;
-		//
-		
 		Object[] array = backupStructure.keySet().toArray();
-//		try {
 			for (int i = 0; i < array.length; i++) {
 				int key = (int) array[i];
 				
-				// DEBIG
-//				lastKey = key;
-//				if (key == 2756) {
-//					System.out.println();
-//				}
-				//
-				
 				// try to remove node with key
 				String deletedValue= rbTree.delete(key);
-//				rbTree.printBetter();
 				String expectedValue = backupStructure.remove(key);
 				assertEquals(expectedValue, deletedValue);
 				
 				// check if tree contains inserted node
 				String foundValue = rbTree.find(key);
-				
-				// DEBIG
-//				if (foundValue != null){
-//					System.out.println("not null");
-//					printList(list);
-//					System.out.println(lastKey);
-//				}
-				//
-				
 				assertNull(foundValue);
 				
 				// check if size is good
-//				try {
-					rbTree.size();
-//				} catch (Exception e) {
-//					System.out.println("size");
-//					printList(list);
-//					System.out.println(lastKey);
-//					throw e;
-//				}
 				assertEquals(rbTree.size(), backupStructure.size());
 				// general check
 				// assertTrue(rbTree.check());
 				
 				// do this that much how much values are in backup structure
 			}
-//		} catch (Exception e) {
-//			System.out.println("last catch");
-//			printList(list);
-//			System.out.println(lastKey);
-//			throw e;
-//		}
 	}
 	
 	private void printList(List<Integer> list) {
